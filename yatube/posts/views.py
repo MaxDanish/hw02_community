507c3969640c1198b56ee69a05b9db06b80b6248
Максим Данишевский <max.danish@yandex.ru>
2022-06-23 01:02:17 +0500

    Теперь база данных с записями пушится и пулится)))

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-# from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 
-def index(request):
+def index(request: HttpResponse) -> HttpRequest:
     template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': 'Yatube',
         'posts': posts
     }
     return render(request, template, context)
@@ -18,7 +17,6 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': 'Группа такая',
         'slug': slug,
         'group': group,
         'posts': posts,
