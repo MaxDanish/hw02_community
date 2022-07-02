4fffe65096e9fcb581f13b66e042349af76c8393
Максим Данишевский <max.danish@yandex.ru>
2022-07-02 14:42:32 +0300

    Персональная страница пользователя и страница записи

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpRequest
 
@@ -43,5 +43,40 @@ def group_posts(request, slug):
         'slug': slug,
         'group': group,
         'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+
+    template = 'posts/profile.html'
+
+    author = get_object_or_404(User, username=username)
+
+    posts = author.posts.all()
+    posts_count = posts.count()
+    paginator = Paginator(posts, POSTS_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'username': username,
+        'posts_count': posts_count,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+
+    template = 'posts/post_detail.html'
+
+    post = get_object_or_404(Post, pk=post_id)
+    posts_count = post.author.posts.count()
+
+    context = {
+        'post_id': post_id,
+        'post': post,
+        'posts_count': posts_count,
     }
     return render(request, template, context)
